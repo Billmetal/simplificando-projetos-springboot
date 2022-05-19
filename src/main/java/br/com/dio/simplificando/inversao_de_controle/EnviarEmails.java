7df0bd6e2dedde513ea137ce6963b95a73588936
Billmetal <billmetal9@gmail.com>
2022-05-18 23:38:34 -0300

    Spring Framework - Inversão de controle .

---
+++ src/main/java/br/com/dio/simplificando/inversao_de_controle/EnviarEmails.java
@@ -0,0 +1,17 @@
+package br.com.dio.simplificando.inversao_de_controle;
+
+public class EnviarEmails {
+
+	public EnviarEmails(String tipo,String endereco,String senha) {
+		
+	}
+	
+	public static EnviarEmails obterDadosEmail() {
+		return new EnviarEmails("stmp", "contato@email.com", "senha");
+	}
+
+	public void retornar(String mensagem) {
+		System.out.println("Email Enviado !");
+	}
+	
+}
