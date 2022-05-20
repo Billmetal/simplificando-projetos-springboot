de0bfb6d025424b559c64300804db7c5a4a8a96d
Billmetal <billmetal9@gmail.com>
2022-05-19 21:15:36 -0300

    Principais dependências e bibliotecas - Feign .

--- src/main/java/br/com/dio/simplificando/SimplificandoApplication.java
+++ src/main/java/br/com/dio/simplificando/SimplificandoApplication.java
@@ -2,8 +2,10 @@ package br.com.dio.simplificando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SimplificandoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/dio/simplificando/feign/ConsumindoApi.java
@@ -0,0 +1,12 @@
+package br.com.dio.simplificando.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "agenda", url = "http://localhost:3000/contato")
+public interface ConsumindoApi {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	Contato retornaContato();
+}
