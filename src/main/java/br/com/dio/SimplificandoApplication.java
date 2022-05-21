29bce3a58e5594fb019e775b3e9788f5de2a463e
Billmetal <billmetal9@gmail.com>
2022-05-20 22:42:33 -0300

    Projeto Springboot - Beans versus Components

--- src/main/java/br/com/dio/SimplificandoApplication.java
+++ src/main/java/br/com/dio/SimplificandoApplication.java
@@ -1,9 +1,15 @@
 package br.com.dio;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+import com.google.gson.Gson;
+
+import br.com.dio.imersao.beans_vs_component.ConversorJson;
+import br.com.dio.imersao.beans_vs_component.ViaCepResponse;
 import br.com.dio.imersao.primeiros_passos.Calculadora;
 
 @SpringBootApplication
@@ -12,6 +18,16 @@ public class SimplificandoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplificandoApplication.class, args);
+	}
+	
+	
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) {
+		return args -> {
+			String json = "{ \"cep\": \"01001-000\", \"logradouro\": \"Praça da Sé\",\"localidade\": \"Centro\" }";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP : "+ response);
+		};
 	}
 
 }
